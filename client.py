ae1deb128a867afaa58301d768c7a0ff5b4cb3f8
哲綸 <s29743191s@gmail.com>
2023-01-06 15:14:14 +0800

    Modify recv into json

--- client.py
+++ client.py
@@ -2,48 +2,70 @@ import socket
 import threading
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ('192.168.115.1', 5050)
-s.connect(addr)
+class ChatClient:
+    def __init__(self) -> None:
+        self.addr = ('192.168.115.1', 5050)
+        self.recv_fromserver = {"text":None, }
+        self.FORMAT = "utf-8"
+    def connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(self.addr)
 
-def countdown():
-    t = 30
-    while t:
-        # mins, secs = divmod(t, 60)
+        print("send 連接成功！現在可以發送消息！\n")
+        while True:
+            if self.recv_fromserver["text"] == "超過了":
+                print("遊戲已滿房")
+                break
+            client_send_msg = input()
+            if client_send_msg == "esc":
+                print("你退出了聊天")
+                sock.close()
+                break
+            recv = threading.Thread(target=self.recv_msg, args=(sock,))
+            recv.start()
+
+            sock.send(client_send_msg.encode("utf-8")) # self.FORMAT
+
+    def recv_msg(self):
+        print("recv連接成功!現在可以接收消息！\n")
+        while True:
+            try: 
+                recv_fromserver = self.sock.recv(1024)
+                recv_fromserver = recv_fromserver.decode("utf-8")
+                self.update_response(recv_fromserver)
+                
+            except ConnectionResetError:
+                print("服務器關閉，聊天已結束！")
+                self.sock.close()
+            if self.recv_fromserver == "超過了":
+                break
+            print(recv_fromserver)
+        # if response == "開始遊戲":
+        #     threading.Thread(target=countdown).start()
         
-        timer = f"00:{t}"
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-    
-
-def recv_msg():
-    print("連接成功！現在可以接收消息！\n")
-    while True:
-        try: 
-            response = s.recv(1024)
-            response = response.decode("utf-8")
-            print(response)
-        except ConnectionResetError:
-            print("服務器關閉，聊天已結束！")
-            s.close()
-        if response == "開始遊戲":
-            threading.Thread(target=countdown).start()
+    def update_response(self,recv_fromserver):
+        self.recv_fromserver = recv_fromserver
+        return self.recv_fromserver
         
+    def countdown():
+        t = 30
+        while t:
+            # mins, secs = divmod(t, 60)
+            
+            timer = f"00:{t}"
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
 
 
-def send_msg():
-    print("連接成功！現在可以發送消息！\n")
-    print("輸入esc來退出聊天")
-    while True:
-        msg = input()
-        if msg == "esc":
-            print("你退出了聊天")
-            s.close()
-            break
-        s.send(msg.encode("utf-8"))
+
+if __name__ == "__main__":
+    client = ChatClient()
+    client.connect()
+    # threads = [, threading.Thread(target=client.send_msg)]
+    # for t in threads:
+    #     t.start()
+
 
             # ↓為什麼這邊是這樣寫↓
-threads = [threading.Thread(target=recv_msg), threading.Thread(target=send_msg)]
-for t in threads:
-    t.start()
+
--- server.py
+++ server.py
@@ -11,6 +11,7 @@ class ChatSever:
         self.users = {}
         self.FORMAT = "utf-8"
         self.answer = "1234"
+        self.__count_client = 0
 
     # def IstwoPlayer(self):
     #     print("正在等待玩家...")
@@ -26,9 +27,8 @@ class ChatSever:
             print('Error!')    
         self.sock.listen(2)
         print("服務器已開啓，等待連接...")
-        self.accept_cont()
         # self.IstwoPlayer()
-
+        self.accept_cont()
 
     def accept_cont(self):
         while True:
@@ -38,26 +38,32 @@ class ChatSever:
             print(f"用戶{addr}連接成功！現在共有{number}位用戶")
             if len(self.users.values()) < 2:
                 s.sendall(f"目前人數 {number} 位，正在等待玩家...".encode(self.FORMAT))
+            elif len(self.users.values()) > 2:
+                s.sendall("超過了".encode(self.FORMAT))
+                self.users.pop(addr)
+                print(f"現在的Client 共有 {len(self.users.keys())}")
             else:
                 for client in self.users.values():
                     client.sendall(f"開始遊戲".encode(self.FORMAT))
-
                 threading.Thread(target=self.recv_send, args=(s, addr)).start()
         
     def recv_send(self, sock, addr):
         while True:
             try:  # 測試後發現，當用戶率先選擇退出時，這邊就會報ConnectionResetError
-                response = sock.recv(4096).decode(self.FORMAT)
-                msg = f"用戶{addr}發來消息：{response}"
-                
-                # print(self.users) # (addr, sock object)   
-                
-                self.broadcast(msg)
+                print("你好 recv")
+                print(self.users)
+                client_response = sock.recv(4096).decode(self.FORMAT)
+                msg = f"用戶{addr}發來消息：{client_response}"
+                print(msg)
+                for client in self.users.values():
+                    client.sendall(msg.encode(self.FORMAT))
             except ConnectionResetError:
                 print(f"用戶{addr}已經退出聊天！")
                 self.users.pop(addr)
                 for client in self.users.values():
                     client.sendall(f"用戶{addr}已經退出聊天！".encode(self.FORMAT))
+    
+    
     # def exit_game(self, msg, addr):
     #     if msg == "esc":
     #         print(f"用戶{addr}已經退出聊天！")
@@ -65,7 +71,7 @@ class ChatSever:
     #         for client in self.users.values():
     #             client.sendall(f"用戶{addr}已經退出聊天！".encode(self.FORMAT))
             
-    def broadcast(self, msg):
+    def broadcast(self, client, msg):
         for client in self.users.values():
             client.sendall(msg.encode(self.FORMAT))
                 
@@ -79,11 +85,14 @@ class ChatSever:
         t = 30
         while t:
             # mins, secs = divmod(t, 60)
-            
             timer = f"00:{t}"
             self.sock.sendall
             time.sleep(1)
             t -= 1
+
+    def count_client(self):
+        self.__count_client = len(self.users.keys())
+        return self.__count_client
 
 if __name__ == "__main__":
     sever = ChatSever()
